689988b09cbe3c9fd2874fa752063659d9821fdd
oliver.xu <929175050@qq.com>
2019-12-06 10:16:54 +0800

    修改文件目录

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # my_opengl_learning
 初学opengl
+# 1. 项目说明
+- 使用`Visual Studio 2019`开发，直接点击`first.sln`打开项目
+- `demo`文件夹内是学习代码
--- demo/2.你好 三角形.cpp
+++ demo/2.你好 三角形.cpp
@@ -2,7 +2,6 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
